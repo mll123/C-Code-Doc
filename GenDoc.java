c595738fe44421725da93171d037f29c1ab32562
Michael L Lomoro <michaellokomo@yahoo.com>
2014-10-25 18:08:19 +0200

    Added functionality to recognise include files and variable declarations

--- GenDoc.java
+++ GenDoc.java
@@ -6,6 +6,7 @@
 
 package scodog;
 import java.util.*;
+import java.util.regex.*;
 
 /**
  *
@@ -15,11 +16,19 @@ public class GenDoc {
     private ArrayList<String> keywordslist;
     private ArrayList<String> datatypeslist;
     private ArrayList<String> functionslist;
+    private boolean mainFunc;
+    private boolean hasBeenMainFunc;
+    public ArrayList<String> includeFileName;
+    public ArrayList<String> variables;
     
     public GenDoc()
     {
         keywordslist=new ArrayList<String>();
         datatypeslist=new ArrayList<String>();
+        includeFileName = new ArrayList<String>();
+        variables = new ArrayList<String>();
+        mainFunc = false;
+        hasBeenMainFunc = false;
         
         /*---------------------------------------------------*/
         if (!(keywordslist.add("if"))){
@@ -50,12 +59,15 @@ public class GenDoc {
             System.out.println("Problem adding keyword - char");
         }
 
-        
+         
     }
     
     boolean isIncludeDirective(String Sline){
         //char sq=S.charAt(0);
         String S[] = Sline.split(" ",2);
+        if (S[0].equals("#include"))
+            includeFileName.add(S[1]);
+        //System.out.println("The length of S is "+S.length);
         return (S[0].equals("#include"));
     }
     
@@ -65,17 +77,52 @@ public class GenDoc {
         return (S[0].equals("int main"));
     }
     
+    boolean isAssignStm(String StrLine)
+    {
+        //String S[] = StrLine.split(" ",0);
+        String PatStr = ".*=.*";
+        Pattern MyPat = Pattern.compile(PatStr);
+        Matcher MyMatch = MyPat.matcher(StrLine);
+        if (MyMatch.matches())
+        {
+                String S[] = StrLine.split("=",2);
+                String T[] = S[0].split(" ",2);
+                variables.add(T[1]);
+        }
+        return MyMatch.matches();
+    }
+    
     public String genText(String CodeLine)
     {
         String myText="False";
-        
-        if (isMainFun(CodeLine))
-        {
-            myText="Doc has main function";
-        }
         if (isIncludeDirective(CodeLine))
         {
-            myText="Doc has directive";
+            myText="Doc has \"include\" directive";
+            myText=myText+" and the include filename is "+includeFileName.get(0);
+        }
+
+        if (isMainFun(CodeLine)) //Hunt for main function
+        {
+            if (!mainFunc) {
+                mainFunc = true;
+                hasBeenMainFunc = true;
+            }
+            else
+            {
+                myText="Error: Double nested main function detected!!";
+            }
+            myText="The main function does the following: ";
+        }
+        
+        if (mainFunc)
+        {
+            if (isAssignStm(CodeLine))
+            {
+                myText = "Assignment statement.";
+                myText = myText + " Variable "+ variables.get(variables.size() - 1);
+                myText = myText + " gets a value.";
+            }
+                    
         }
         return myText;
     }
